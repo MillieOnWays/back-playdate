dc28c77ffebb7967b19351637f50d88d6bcc3f51
cynster <marius.korff@live.nl>
2022-01-28 10:05:31 +0100

    merged playdates routers

--- routers/playdates.js
+++ routers/playdates.js
@@ -29,4 +29,47 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.post("/:id", async (req, res) => {
+  const {
+    name,
+    date,
+    startTime,
+    endTime,
+    address,
+    city,
+    imageUrl,
+    tag,
+    description,
+  } = req.body;
+  if (
+    !name ||
+    !date ||
+    !startTime ||
+    !address ||
+    !city ||
+    !tag ||
+    !description
+  ) {
+    return res.status(400).send("Please provide all required fields");
+  }
+  try {
+    const newPlaydate = await Playdate.create({
+      name,
+      date,
+      startTime,
+      endTime,
+      address,
+      city,
+      tag,
+      description,
+      userId: req.params.id,
+    });
+    console.log("new playdate", newPlaydate);
+    res.send(newPlaydate);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
 module.exports = router;
