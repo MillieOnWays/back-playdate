af7b4832c2b485717cf68dff2ddc638c53d1f5d4
deepali-02 <deepali93.redekar@gmail.com>
2022-02-01 14:05:48 +0100

    update seeder file for playdate

--- seeders/20220127111021-playdate.js
+++ seeders/20220127111021-playdate.js
@@ -10,7 +10,8 @@ module.exports = {
           date: "2022-02-07",
           startTime: new Date(),
           endTime: new Date(),
-          description: "Let's go to Pathe, Dora the explorer",
+          description:
+            "We are planning to go to cinema on 7th Feb 2022. We would love if some of you guys can join us with your kids. Let's go to Pathe, Dora the explorer. We can meet at Pathe at 11.00 am",
           city: "Amsterdam",
           address: "Codaisseurstraat 8",
           tag: "cinema",
@@ -23,10 +24,11 @@ module.exports = {
           date: "2022-03-01",
           startTime: new Date(),
           endTime: new Date(),
-          description: "Let's go to the farm, Dora the horse rider",
+          description:
+            "Let's go to the farm, There kids can spend time with farm animals and nature as well. It will be very good opportunity for kids to explore the nature. So please join us on 1st March, 2022. We can start at 15.00. Let's have fun together.",
           city: "Almere",
           address: "Almerestraat 19",
-          tag: "horses",
+          tag: "farm",
           createdAt: new Date(),
           updatedAt: new Date(),
           userId: 2,
